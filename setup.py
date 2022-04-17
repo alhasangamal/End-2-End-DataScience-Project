7a87b117fb3b2c66d790c0463ebe6066fe36c850
alhasangamal <alhasangamal19@gmail.com>
2022-04-17 17:52:08 +0200

    First Commite

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This a End to end project Datascience in ITI AI-pro intake 2',
+    author='Alhasan Gamal',
+    license='MIT',
+)
